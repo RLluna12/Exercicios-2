434936aaecb0c84ada828c229fc807fdc6fdf23e
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-24 23:45:18 -0300

    1-media numeros negativos

---
+++ 1-media numeros negativos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double i = 0, num, negat = 0;
+            double media = 0;
+
+            while (i < 10)
+            {
+                Console.WriteLine("Digite um Numero: ");
+                num = double.Parse(Console.ReadLine());
+                i++;
+
+                if (num < 0)
+                {
+                    negat = num + negat;
+                    Console.WriteLine("Numero negativo Digitado" + num);
+                    media = negat / i;
+
+
+                }
+
+
+
+            }
+            Console.WriteLine("Soma dos numeros negativos: " + negat);
+            Console.WriteLine("MEdia: " + media);
+        }
+    }
+}
