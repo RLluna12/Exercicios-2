436d1a4c89fcadb4b3bab0e5b00de32936d23879
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-28 22:17:36 -0300

    quadrado de 10 numeros

---
+++ 5- quadrado de 10 numeros1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] y = new int[10];
+            int[] x = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Digite um número: ");
+                x[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+               
+                Console.WriteLine("Vetor 1:{0} ", x[i] );
+            }
+
+            Console.WriteLine(" ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                y[i] = x[i] * x[i];
+                Console.WriteLine("Vetor 2: {0} ", y[i] );
+            }
+        }
+    }
+}
