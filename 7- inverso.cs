aaab6dbf67e578878b9334da19e2a5801331ad1a
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-10-01 22:37:09 -0300

    7- inverso

---
+++ 7- inverso.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] v = new int[6];
+            for(int i=0; i<6; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Inverso do q foi digitado: ");
+            for (int i = 5; i >= 0; i--)
+            {
+                
+                Console.WriteLine(v[i]);
+            }
+        }
+    }
+}
