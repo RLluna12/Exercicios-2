52d79e4d6021c185cce22b9cfe5f396d4f84773a
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-10-01 22:56:43 -0300

    Update 8-Media geral da sala.cs

--- 8-Media geral da sala.cs
+++ 8-Media geral da sala.cs
@@ -11,15 +11,20 @@ namespace ConsoleApp31
         static void Main(string[] args)
         {
             int[] v = new int[15];
-            int media=0;
-            for(int i=0; i<15; i++)
+            int media = 0;
+            for (int i = 0; i < 15; i++)
             {
                 Console.Write("Nota: ");
-
-               v[i] = int.Parse(Console.ReadLine());
+                v[i] = int.Parse(Console.ReadLine());
+                if (v[i] > 10)
+                {
+                    Console.WriteLine("erro");
+                    v[i] = v[15];
+                }
+               
                 media += v[i];
             }
-            Console.WriteLine("Media geral: "+media/15);
+            Console.WriteLine("Media geral: " + media / 15);
         }
     }
 }
