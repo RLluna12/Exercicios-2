126802746b8ba3bc5100a90271a6abfe5d6a4724
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-10-01 22:50:52 -0300

    8-Media geral da sala

---
+++ 8-Media geral da sala.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[15];
+            int media=0;
+            for(int i=0; i<15; i++)
+            {
+                Console.Write("Nota: ");
+
+               v[i] = int.Parse(Console.ReadLine());
+                media += v[i];
+            }
+            Console.WriteLine("Media geral: "+media/15);
+        }
+    }
+}
